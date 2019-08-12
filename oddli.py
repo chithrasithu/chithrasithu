3e0c9fbb9b477e3f8c285cf920f1b781cb4c5b3c
chithrasithu <52281748+chithrasithu@users.noreply.github.com>
2019-08-12 16:16:14 +0530

    Create oddli.py

---
+++ oddli.py
@@ -0,0 +1,9 @@
+start, end = map(int,input(" ").split())
+#end = int(input("Enter the end of range: ")) 
+  
+# iterating each number in list 
+for num in range(start+1, end): 
+      
+    # checking condition 
+    if num % 2 != 0: 
+        print(num, end = " ") 
