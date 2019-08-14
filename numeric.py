4b4e6be10a5bf56199c140eb519bf4351383dfa4
chithrasithu <52281748+chithrasithu@users.noreply.github.com>
2019-08-14 15:54:16 +0530

    Create numeric.py

---
+++ numeric.py
@@ -0,0 +1,17 @@
+def Count(str): 
+    upper, lower, number, special = 0, 0, 0, 0
+    for i in range(len(str)): 
+        if str[i] >= 'A' and str[i] <= 'Z': 
+            upper += 1
+        elif str[i] >= 'a' and str[i] <= 'z': 
+            lower += 1
+        elif str[i] >= '0' and str[i] <= '9': 
+            number += 1
+        else: 
+            special += 1
+   
+    print(number) 
+    
+# Driver Code 
+str = input(" ")
+Count(str) 
