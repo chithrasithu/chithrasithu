32bd7a45cc28b08f343e68255a23d2de9228612f
chithrasithu <52281748+chithrasithu@users.noreply.github.com>
2020-01-02 22:20:34 +0530

    Create array 8 copying array to new array.py

---
+++ array 8 copying array to new array.py
@@ -0,0 +1,10 @@
+#write a program to copy all element from one array to another array
+l=list(map(int,input().split()))
+newl=[]
+for i in l:
+  newl.append(i)
+for j in newl:    
+  print(j,end=" ")
+
+  
+#codded by chithra
