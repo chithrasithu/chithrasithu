7b4fd3c764e73060de8b19a3922e1134d5dc4ac7
chithrasithu <52281748+chithrasithu@users.noreply.github.com>
2020-01-03 07:38:59 +0530

    Create loop 8 sum of odd num.py

---
+++ loop 8 sum of odd num.py
@@ -0,0 +1,10 @@
+#write a program to print sum of odd  numbers
+n = int(input())
+i=2
+sum1=0
+while(i<=n):
+  if i % 2 != 0:
+    sum1=sum1+i
+  i  = i + 1
+print("sum of odd num", sum1)
+#codded by chithra
