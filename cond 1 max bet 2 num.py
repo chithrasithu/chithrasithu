d303e15549e4842b50118c575a2986b489586c48
chithrasithu <52281748+chithrasithu@users.noreply.github.com>
2020-01-05 19:18:37 +0530

    Create cond 1 max bet 2 num.py

---
+++ cond 1 max bet 2 num.py
@@ -0,0 +1,10 @@
+#write the code to print the gretest number between the 2 numbers
+print("enter the number")
+num1,num2 = map(int,input().split())
+if num1 > num2:
+  print(num1," is greater")
+else:
+  print(num2," is grater")
+
+
+# codded by chithra
