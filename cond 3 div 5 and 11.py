97f0ad58523b40c8895efcbf96fc394d34e54550
chithrasithu <52281748+chithrasithu@users.noreply.github.com>
2020-01-06 09:49:59 +0530

    Create cond 3 div 5 and 11.py

---
+++ cond 3 div 5 and 11.py
@@ -0,0 +1,8 @@
+#write a code to find wheather the number id div by 5 and 11
+print("enter the number")
+in1 = int(input())
+if in1 % 5 == 0 and in1 % 11 == 0 :
+  print(in1,"it's divisible by both 5 and 11")
+else:
+  print("it's not divisible by 5 and 11")
+#codded by chithra
